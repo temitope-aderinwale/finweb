fe1d076a8524044022eb8db80d4b0e3bbf313477
Dennis Aderinwale <aderinwaledennis@outlook.com>
2025-04-19 19:29:52 +0100

    first commit

---
+++ api/Interfaces/IStockRepository.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Stock;
+using api.Helpers;
+using api.Models;
+
+namespace api.Interfaces
+{
+    public interface IStockRepository
+    {
+        Task<List<Stock>> GetAllAsync(QueryObject query);
+
+        //FirstOrDefault CAN BE NULL
+        Task<Stock?> GetByIdAsync(int id);
+        Task<Stock?> GetBySymbolAsync(string symbol);
+        Task<Stock> CreateAsync(Stock stockModel);
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
+        Task<Stock?> DeleteAsync(int id);
+        Task<bool> StockExists(int id);
+    }
+}
---
+++ api/Models/Comment.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+  [Table("Comments")]
+  public class Comment
+  {
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Content { get; set; } = string.Empty;
+    
+    public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+    // Navigation Property - Navigation Id
+    public int? StockId { get; set; }
+    
+    
+    public Stock? Stock { get; set; }
+
+    public string AppUserId { get; set; }
+
+    public AppUser AppUser { get; set; }
+  }
+}
